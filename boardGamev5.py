aee11fc3234aec78f711080450f1211eff1694c3
hsintingho <yama762682@gmail.com>
2019-01-23 12:19:47 -0500

    Final version

--- boardGamev5.py
+++ boardGamev5.py
@@ -25,17 +25,30 @@
 
 
 from graphics import *
+from random import shuffle
 
 def main():
 
     # Declare and initialize variables
 
-    intro = name = choice = ""
+    intro = name = choice = cardTitle = question = ""
     playAgain = finishedRules = "yes"
     position = 0
     invalid = True
-    fileName = "card.txt"
+    fileName = "cards.txt"
+    cardwidth = 500
+    cardheigth = 300
+    cardButtonRX = 200
+    cardButtonLX = 300
+    cardButtonUY = 200
+    cardButtonDY = 250
+    questionX = 250
+    questionY = 150
     # Define menu
+
+    #function for finding the middle point to place text
+    def getMidPoint(p1, p2):
+        return p1+(p2-p1)/2
 
     def displayMenu():
         print('\n1. See Rules')
@@ -52,7 +65,7 @@ def main():
         ruleWin = GraphWin("Rules", 300, 200)
         rule = Text(Point(60, 70), msg)
         rule.draw(ruleWin)
-
+    
     # Prompt user for their name
     nameWin = GraphWin("User Name", 500, 250)
     inputEntry = Entry(Point(350, 100), 10)
@@ -84,68 +97,54 @@ def main():
             displayText.draw(nameWin)
             time.sleep(3)
             nameWin.close()
+    
     # Introduction and greeting window
-
-    intro = "\nWelcome to the FBI programmer game!\n"
+    
+    intro = name + "\nWelcome to the FBI programmer game!\n"
     introWin = GraphWin("FBI Programmer", 720, 400)
-    ruleButton = Rectangle(Point(100, 250), Point(200, 300))
-    playButton = Rectangle(Point(300, 250), Point(400, 300))
-    exitButton = Rectangle(Point(500, 250), Point(600, 300))
-    #background = Image(Point(0,0), "fbi.gif")
-    #background.draw(introWin)
+    ruleButton = Rectangle(Point(100, 300), Point(200, 350))
+    playButton = Rectangle(Point(300, 300), Point(400, 350))
+    exitButton = Rectangle(Point(500, 300), Point(600, 350))
+    ruleButtonText = Text(Point(150, 325), "Show Rules")
+    playButtonText = Text(Point(350, 325), "Play")
+    exitButtonText = Text(Point(550, 325), "Exit")
+    greeting = Text(Point(350,250), intro)
+    background = Image(Point(360,100), "fbi.gif")
+    background.draw(introWin)
     ruleButton.draw(introWin)
     playButton.draw(introWin)
     exitButton.draw(introWin)
+    ruleButtonText.draw(introWin)
+    playButtonText.draw(introWin)
+    exitButtonText.draw(introWin)
+    greeting.draw(introWin)
 
+    mainScreen = True
+    while mainScreen:
+        buttonClick = introWin.getMouse()
+        clickXPos = buttonClick.getX()
+        clickYPos = buttonClick.getY()
 
-
-
-
-
-
-
-
-'''
-    while playAgain == "yes":# This is our loop after checking for user input
-
-        while invalid:
-            try:
-                displayMenu()
-                choice = int(input("What would you like to do? (1/2/3): "))
-                assert 1<=choice<=3
-                invalid = False
-            except ValueError:
-                print("\nNot a valid choice. Please try again.\n")
-            except AssertionError:
-                print("\nPlease enter a number between 1 ~ 3.\n")
-
-
-        try:
-            if choice == 1:
-                showRule()
-
-                # Prompt to see if they're finished reading the rules
-
-                #finishedRules = input("Have you finished reviewing the rules?(yes/no): ").lower()
-                #if finishedRules == "yes":
-                    #invalid = True
-
-                #else:
-                    #continue
-
-            # Start game. All answers are inputted as strings
-
-            # Display position after each answer
-
-            elif choice == 2:
-                #opens the file contains cards
-                fileName = "cards.txt"
-                answer = ""
+        if 300 <= clickYPos <= 350:
+            if 100 <= clickXPos <= 200:
+                #rule window
+                cardTitle = "Rules"
+                msg = '\n\nHello, programmer! \nThe FBI needs your assistance to catch a hacker.\n'
+                msg += '14 Python questions will be shown in cards randomly.\n'
+                msg += 'If you give the right answer,\nyou can move forward; otherwise, move backward.\n'
+                msg += '\nYou will win the game after you have moved forward 20 steps.\n\n'
+                cardWin = GraphWin(cardTitle, cardwidth, cardheigth)
+                rule = Text(Point(cardwidth/2, cardheigth/2), msg)
+                rule.draw(cardWin)
+            
+            #when play is clicked
+            if 300 <= clickXPos <= 400:
                 position = 0
                 fileExist = True
                 qList = []
                 pList = []
                 aList = []
+                
                 try:
                     #open the card file
                     cards = open(fileName, "r").readlines()
@@ -156,50 +155,155 @@ def main():
                         pList.append(card[1])
                         aList.append(card[2])
 
-                    for x in range(len(qList)):
-                        answer = input(qList[x])
-                        try:
-                            answer = answer.lower()
-                        except:
-                            pass
-                        #print(aList[x].strip())
-                        if answer == aList[x].strip():
-                            position += int(pList[x])
-                            print("\nAdvance " ,pList[x], "steps!")
-                        else:
-                            print("You typed: ", answer, "\nCorrect answer:", aList[x])
-                            position -= int(pList[x])
-                            print("Retreat ", pList[x], " steps.")
-                        print("You are currently at position ", position,"\n")
+                    #shuffle a list of number between 0 and the size of the list
+                    indexList = [index for index in range(len(qList))]
+                    shuffle(indexList)
+                    print(indexList)
+                    
+                    #loop through shuffled list to get random number of question
+                    for x in indexList:
+                        #make card
+                        print(x)
+                        cardTitle = "Question"
+                        cardWin = GraphWin(cardTitle, cardwidth, cardheigth)
 
+                        #swich line at 5th word to keep text in the window
+                        
+                        print(qList[x])
+                        question = ""
+                        word = qList[x].split(" ")
+                        for i in range(len(word)):
+                            
+                            question += word[i] + " "
+                            if i == 5:
+                                question += "\n"
+                            else:
+                                continue
+                                    
+
+                        text = Text(Point(250, 100), question)
+                        answerField = Entry(Point(cardwidth/2, 5+cardheigth/2), 10)
+                        yesButton = Rectangle(Point(cardButtonLX, cardButtonUY), Point(cardButtonRX, cardButtonDY))
+                        yesText = Text(Point(getMidPoint(cardButtonLX, cardButtonRX), getMidPoint(cardButtonUY, cardButtonDY)), "Submit")
+                        text.draw(cardWin)
+                        yesButton.draw(cardWin)
+                        yesText.draw(cardWin)
+                        answerField.draw(cardWin)
+
+                        submit = True                            
+                        while submit:
+                            buttonClick = cardWin.getMouse()
+                            clickXPos = buttonClick.getX()
+                            clickYPos = buttonClick.getY()
+
+                            if  200 <= clickXPos <= 300 and 200 <= clickYPos <= 250:
+                                try:
+                                    answer = answerField.getText().lower()
+                                except:
+                                    print("check answer input")
+
+                                #Display result
+                                cardTitle = "Result"
+                                resultWin = GraphWin(cardTitle, cardwidth, cardheigth)
+                                
+                           
+                                yesButton = Rectangle(Point(cardButtonLX, cardButtonUY), Point(cardButtonRX, cardButtonDY))
+                                yesText = Text(Point(getMidPoint(cardButtonLX, cardButtonRX), getMidPoint(cardButtonUY, cardButtonDY)), "Next")
+                                
+                                if answer == aList[x].strip():
+                                    position += int(pList[x])
+                                    text = "Advance " +pList[x]+ " steps!"
+                                else:
+                                    position -= int(pList[x])
+                                    text = "You typed: "+ answer+ "\nCorrect answer: "+ aList[x]
+                                    text += "Retreat "+ pList[x]+ " steps."
+                                    
+                                text += "You are currently at position "+ str(position)+"\n"
+                                msg = Text(Point(cardwidth/2, cardheigth/2), text)
+                                msg.draw(resultWin)
+                                yesButton.draw(resultWin)
+                                yesText.draw(resultWin)
+
+                                nextCard = True
+                                while nextCard:
+                                    buttonClick = resultWin.getMouse()
+                                    clickXPos = buttonClick.getX()
+                                    clickYPos = buttonClick.getY()
+                                    if  200 <= clickXPos <= 300 and 200 <= clickYPos <= 250:
+                                        resultWin.close()
+                                        cardWin.close()
+                                        break
+                                break #break while submit
+                       
                     #Display results
+                    cardWin = GraphWin(cardTitle, cardwidth, cardheigth)
                     if position >= 20:
-                        print("Position: ", position)
-                        print("\nCongratulations, ", name, "! You won!")
-
+                        text = "Position: "+ str(position) +"\n"+ "\nCongratulations, "+ name+ "! You won!"
+                        
                     else:
-                        print("Position: ", position)
-                        print("\nI'm sorry, ", name,", you didn't quite make it.")
-                        print("Better luck next time!")
+                        text = "Position: "+ str(position)+"\n"+"\nI'm sorry, "+ name+", you didn't quite make it."+"\nBetter luck next time!"
+                    text += "\nAre you finished? Would you like to play again?"
+                    yesButton =  Rectangle(Point(100, 250), Point(200, 280))
+                    noButton = Rectangle(Point(300, 250), Point(400, 280))
+                    yesText = Text(Point(150, 265), "Yes")
+                    noText = Text(Point(350, 265), "No")
+                    msg = Text(Point(cardwidth/2, cardheigth/2), text)
+                    msg.draw(cardWin)
+                    yesButton.draw(cardWin)
+                    noButton.draw(cardWin)
+                    yesText.draw(cardWin)
+                    noText.draw(cardWin)
 
-                    position = 0
-                    playAgain = input("\nAre you finished? Would you like to play again, yes or no? ")
-                except:
+                    keepPlay = True
+                    while keepPlay:
+                        buttonClick = cardWin.getMouse()
+                        clickXPos = buttonClick.getX()
+                        clickYPos = buttonClick.getY()
+                        if 250 <= clickYPos <= 280:
+                            if 100 <= clickXPos <= 200:
+                                print("yes!!!")
+                                position = 0
+                                keepPlay = False
+                                cardWin.close()
+                                break
+                            if 300<= clickXPos <= 400:
+                                print("no!!!")
+                                keepPlay = False
+                                cardWin.close()
+                                mainScreen = False
+                                introWin.close()
+                    
+                
+                    
+                    
+                except  OSError:
                     print("File: {} does not exist. \nThe game can't continue without the file.".format(fileName))
                     print("Program is shutting down...... See you when you get the right file. Bye!")
 
-                ###########################################################################
+            #when exit is clicked
+            if 500 <= clickXPos <= 600:
+                cardTitle = "Exit"
+                msg = "Are you sure you want to quit?"
+                cardWin = GraphWin(cardTitle, cardwidth, cardheigth)
+                text = Text(Point(cardwidth/2, cardheigth/2), msg)
+                yesButton = Rectangle(Point(cardButtonLX, cardButtonUY), Point(cardButtonRX, cardButtonDY))
+                yesText = Text(Point(getMidPoint(cardButtonLX, cardButtonRX), getMidPoint(cardButtonUY, cardButtonDY)), "Yes")
+                text.draw(cardWin)
+                yesButton.draw(cardWin)
+                yesText.draw(cardWin)
 
-            elif choice == 3:
-                print("Goodbye! See you next time~")
-                playAgain = "no"
+                stay = True
+                while stay:
+                    buttonClick = cardWin.getMouse()
+                    clickXPos = buttonClick.getX()
+                    clickYPos = buttonClick.getY()
+                    if 200 <= clickXPos <= 300 and 200 <= clickYPos <= 250:
+                        print("right here!")
+                        stay = False
+                        cardWin.close()
+                        mainScreen = False
+                        introWin.close()
+                        
 
-        except:
-            print("Something went wrong. The game will restart.")
-            main()
 
-    # Solutation
-
-    print("Thanks for playing!")
-'''
 main()
